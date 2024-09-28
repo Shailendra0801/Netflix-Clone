7f5a950fa704ae9529761c436d90f684f3cba093
Shailendra <shailendrabisht69@gmail.com>
2024-09-28 19:06:59 +0530

    Still on it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Route, Switch } from "react-router";
 import { JumbotronContainer } from './containers/jumbotron'
+import { FooterContainer } from "./containers/footer";
+import { FaqsContainer } from "./containers/faqs";
+import * as ROUTES from './constants/routes'
 
 export default function App() {
-  return <JumbotronContainer/>;
+  return <>
+    <JumbotronContainer />
+    <FaqsContainer />
+    <FooterContainer />
+  </>;
 }
 
---
+++ src/constants/routes.js
@@ -0,0 +1,4 @@
+export const HOME = '/';
+export const Browse = './browse';
+export const SIGN_UO = '/signup';
+export const SIGN_IN = '/signin';
